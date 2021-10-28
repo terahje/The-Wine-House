413a3e567d51c0b39f09889294ec7771da1280c9
terahje <terahjequinn@gmail.com>
2021-10-27 20:23:49 -0700

    update to responsive and js features

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ let counter1 = 0;
 let counter2 = 1;
 let bool = true;
 
-const sections = document. querySelectorAll("section");
-const progress = document.querySelector('.progress h2');
-const circles = document.querySelectorAll('.circle')
+const sections = document.querySelectorAll("section");
+const progress = document.querySelector(".progress h2");
+const circles = document.querySelectorAll(".circle");
+const menu = document.querySelector(".menu");
+const section1wrapper = document.querySelector(".section-1-wrapper");
+const section5wrapper = document.querySelector(".section-5-wrapper");
+
+section1wrapper.style.transform = "scale(1)";
 
 const progressCounter = () => {
     progress.textContent = `${counter2}/${sections.length}`;
@@ -23,6 +28,8 @@ const pageController = () => {
         })
         counter1 = 0;
         counter2 = 1;
+        section1wrapper.style.transform = "scale(1)";
+        section5wrapper.style.transform = "scale(1.5)";
         progressCounter();
         bool = false;
     }
@@ -36,6 +43,8 @@ const pageController = () => {
         })
         counter1 = '4';
         counter2 = '5';
+        section1wrapper.style.transform = "scale(1.5)";
+        section5wrapper.style.transform = "scale(1)";
         progressCounter();
         bool = false;
     }
@@ -58,24 +67,53 @@ pageController();
 progressCounter();
 console.log(counter1, counter2);
 
-bool && (document.querySelector(`.section-${deltaY ? counter1 : counter2}`).style.left = `${deltaY ? "-100vw" : "0"}`);
+if (bool) {
+    document.querySelector(
+      `.section-${deltaY ? counter1 : counter2}`
+    ).style.left = `${deltaY ? "-100vw" : "0"}`;
 
+    document.querySelector(
+      `.section-${deltaY ? counter1 : counter2}-wrapper`
+    ).style.transform = `scale(${deltaY ? "1.5" : "1"})`;
+
+    document.querySelector(
+      `.section-${deltaY ? counter1 + 1 : counter2 + 1}-wrapper`
+    ).style.transform = `scale(${deltaY ? "1" : "1.5"})`;
+  }
 });
 
 document.querySelector('.left-btn').addEventListener('click', () => {
     counter1--;
     counter2--;
     pageController() && (document.querySelector(`.section-${counter2}`).style.left = '0');
+
+    if (bool) {
+        document.querySelector(`.section-${counter2}-wrapper`).style.transform =
+          "scale(1)";
+        document.querySelector(`.section-${counter2 + 1}-wrapper`).style.transform =
+          "scale(1.5)";
+      }
 });
 document.querySelector('.right-btn').addEventListener('click', () => {
     counter1++;
     counter2++;
     pageController() && (document.querySelector(`.section-${counter1}`).style.left = '-100vw');
+
+    if (bool) {
+        document.querySelector(`.section-${counter2}-wrapper`).style.transform =
+          "scale(1)";
+        document.querySelector(`.section-${counter1}-wrapper`).style.transform =
+          "scale(1.5)";
+      }
 });
 
 document.querySelector('.grapes-img').addEventListener('mouseover', () => {
     document.querySelector('.section-3-wrapper').style.opacity = '.5';
-})
+});
 document.querySelector('.grapes-img').addEventListener('mouseout', () => {
     document.querySelector('.section-3-wrapper').style.opacity = '1';
-})
+});
+
+menu.addEventListener("click", () => {
+    document.querySelector(".navbar").classList.toggle("change");
+});
